7dd8f1b73713387676852aa8d7454f0baef44c1c
micpilar <146476283+micpilar@users.noreply.github.com>
2023-10-15 16:50:51 +0200

    Update polynomial.cpp

--- polynomial.cpp
+++ polynomial.cpp
@@ -1,3 +1,23 @@
+// Implementacja modułu "polynomial".
+//
+// Copyright (C) 2023 by Michał Pilarczyk.
+//
+// This is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License
+// as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this software; see the file "LICENSE". If not,
+// please, see <https://www.gnu.org/licenses/>.
+
+// Started on 14.10.2023. Last revision: 15.10.2023.
+
 #include <iostream>
 #include <stdexcept>
 #include "polynomial.h"
