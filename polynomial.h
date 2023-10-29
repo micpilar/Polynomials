c6ee898c1ac97335afe378b84018e53ff2dc4594
micpilar <146476283+micpilar@users.noreply.github.com>
2023-10-29 14:18:14 +0100

    Added dividing polynomials

--- polynomial.h
+++ polynomial.h
@@ -16,23 +16,29 @@
 // along with this software; see the file "LICENSE". If not,
 // please, see <https://www.gnu.org/licenses/>.
 
-// Started on 14.10.2023. Last revision: 15.10.2023.
+// Started on 14.10.2023. Last revision: 29.10.2023.
 
 #pragma once
 #include <vector>
+#include <cmath>
 
 // Wielomian jest reprezentowany przez vector<double> który zawiera współczynniki wielomianu od a0 do an. Przykładowo [1,3,6] znaczy 1+3x+6x^2.
 // UWAGA: wielomian całkowicie zerowy (w=0) ma stopień -nieskończoność, jednak w tym programie output musi być liczbą całkowitą, więc nadam mu stopień -1.
 
 typedef std::vector<double> poly;
+typedef std::vector<int> intpoly;
 
 
 poly& minimize(poly &b); //minimalizuje reprezentację wielomianu (usuwa zera z tyłu)
 int deg(const poly &a); //zwraca stopień wielomianu
+double evaluate(const poly &a, double x); //zwraca wartość wielomianu w punkcie x
+intpoly convert(const poly &a); //konwertuje vector z double na int
+poly unconvert(const intpoly &a); //konewrtuje vector z int na double
 poly add(const poly &c,const poly &d); //zwraca wektor będący reprezentacją wielomianu sumy dwóch wielomianów
 poly sub(const poly &e,const poly &f); //zwraca wektor będący reprezentacją wielomianu różnicy dwóch wielomianów
 poly mul(const poly &g,const poly &h); //zwraca wektor będący reprezentacją wielomianu mnożenia dwóch wielomianów
 poly lagrange(const std::vector<double> x, const std::vector<double> y); //zwraca wektor będący reprezentacją wielomianu interpolyjnego obliczonego według wzorów Lagrange'a; wartości wielomianu w punktach x[i] wynoszą y[i]
+poly Horner(const poly &a, double c); //zwraca wynik dzielenia wielomianu a przez wielomian b; wszystkie ich współczynniki muszą być całkowite
 
 template <typename STREAM>
 STREAM &operator<<(STREAM &s, const poly &p)
@@ -43,7 +49,10 @@ STREAM &operator<<(STREAM &s, const poly &p)
       continue;
     if(wypisane)
       s<<"+";
-    s<<p[i];
+    if (p[i]<0)
+      s<<"("<<p[i]<<")";
+    else
+      s<<p[i];
     if (i>1)
       s<<"*x^"<<i;
     if(i==1)
